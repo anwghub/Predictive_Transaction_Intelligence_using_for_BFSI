d76aa681d5ed3d4c31fdc1f652bc60640fcdc6d6
Anwesa Ghosh <anwesa2021@gmail.com>
2025-11-28 14:08:29 +0530

    added processed, eda, graphs

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+from flask import Flask
 from src.preprocessing import run_processing
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Flask app is running!"
+
+@app.route("/process")
+def process():
+    run_processing()
+    return "Processing completed!"
 
 if __name__ == "__main__":
     run_processing()
+    
+    # Run Flask app on port 8000
+    app.run(host="0.0.0.0", port=8000, debug=True)
